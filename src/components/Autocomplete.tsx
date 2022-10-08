5eab7b2e34d50143d910eef4cea1333ac9bbf4c9
Adriana Morales <amorales25@gmail.com>
2022-10-08 14:10:46 -0400

    added comments to Autocomplete.

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,10 +1,12 @@
+// Autocomplete component - is used in form to autocomplete names of existing members as users type them.
+
 type Props = {
   name: string;
   userInput: string;
   id: string;
   onClickEvent: (name: string) => void;
 };
-
+//is exported and used in create. Checks that user input & existing name in database match as lowercase strings.
 export const Autocomplete = ({ name, userInput, id, onClickEvent }: Props) => {
   const userNameLowercase = name.toLowerCase();
   const userInputLowercase = userInput.toLowerCase();
