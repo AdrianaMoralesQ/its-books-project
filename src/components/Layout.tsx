93f7c944c7e00ec31620ad2c95b9ca4bbc703fda
Adriana Morales <amorales25@gmail.com>
2022-10-08 14:30:37 -0400

    Added missing comments to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,8 @@ import { useContext } from 'react';
 import { Loading } from 'components/Loading';
 import styled from 'styled-components';
 import { Navbar } from './Navbar';
-
+// layout is the body/container of all site.
+// has "loading" state
 const Wrapper = styled.div``;
 
 export const Layout = ({ children }: any) => {
